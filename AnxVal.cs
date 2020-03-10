c362b6ce3daaeaf82c003f6667be7442b68bb70f
Oqqay <59747577+Oqqay@users.noreply.github.com>
2020-03-09 22:20:30 -0700

    Update AnxVal.cs

--- AnxVal.cs
+++ AnxVal.cs
@@ -6,7 +6,6 @@ public class AnxVal : MonoBehaviour
 {
     public float anxietyLv;
     public AnxietyBar ABar;
-    public GameObject Hud;
     // Start is called before the first frame update
     void Start()
     {
